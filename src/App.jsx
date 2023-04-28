d8fddebd7051c4ad7089c3915c6cdc60c9a894cd
Gr4childo <samohkin.2012@gmail.com>
2023-04-29 02:24:20 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,66 @@
 import { useState, useEffect, useRef } from 'react';
 
 const formatTimer = (time) => {
-	let hour = Math.floor(time / (60 * 60)),
-		resultMinutes = time % (60 * 60),
-		minute = Math.floor(resultMinutes / 60),
-		resultSeconds = resultMinutes % 60,
-		second = Math.ceil(resultSeconds),
-		fullTime;
+  let hour = Math.floor(time / (60 * 60)),
+    resultMinutes = time % (60 * 60),
+    minute = Math.floor(resultMinutes / 60),
+    resultSeconds = resultMinutes % 60,
+    second = Math.ceil(resultSeconds),
+    fullTime;
 
-	if (second === 60) {
-		second = 0;
-		minute = minute + 1;
-	}
-	if (second < 10) {
-		second = '0' + second;
-	}
-	if (minute === 60) {
-		minute = 0;
-		hour = hour + 1;
-	}
-	if (minute < 10) {
-		minute = '0' + minute;
-	}
-	if (hour < 10) {
-		hour = '0' + hour;
-	}
+  if (second === 60) {
+    second = 0;
+    minute = minute + 1;
+  }
+  if (second < 10) {
+    second = '0' + second;
+  }
+  if (minute === 60) {
+    minute = 0;
+    hour = hour + 1;
+  }
+  if (minute < 10) {
+    minute = '0' + minute;
+  }
+  if (hour < 10) {
+    hour = '0' + hour;
+  }
 
-	fullTime = hour + ':' + minute + ':' + second;
+  fullTime = hour + ':' + minute + ':' + second;
 
-	return fullTime;
+  return fullTime;
 };
 
 const App = () => {
-	const inputRef = useRef();
-	const [timer, setTimer] = useState();
-	const timerIntervalId = useRef();
+  const inputRef = useRef();
+  const [timer, setTimer] = useState();
+  const timerIntervalId = useRef();
 
-	useEffect(() => {
-		timerIntervalId.current = setInterval(() => {
-			setTimer(timer - 1);
-		}, 1000);
-		return () => clearInterval(timerIntervalId.current);
-	}, [timer]);
+  useEffect(() => {
+    timerIntervalId.current = setInterval(() => {
+      setTimer(timer - 1);
+    }, 1000);
+    return () => clearInterval(timerIntervalId.current);
+  }, [timer]);
 
-	useEffect(() => {
-		if (timer <= 0) {
-			clearInterval(timerIntervalId.current);
-		}
-	}, [timer]);
+  useEffect(() => {
+    if (timer <= 0) {
+      clearInterval(timerIntervalId.current);
+    }
+  }, [timer]);
 
-	return (
-		<>
-			<input placeholder='Seconds' type='text' ref={inputRef} />
+  return (
+    <>
+      <input placeholder='Seconds' type='text' ref={inputRef} />
 
-			<button onClick={() => setTimer(inputRef.current.value)}>Start</button>
+      <button onClick={() => setTimer(inputRef.current.value)}>Start</button>
 
-			<br />
-			<br />
+      <br />
+      <br />
 
-			<span>{timer ? formatTimer(timer) : 'hh:mm:ss'}</span>
-		</>
-	);
+      <span>{timer ? formatTimer(timer) : 'hh:mm:ss:'}</span>
+    </>
+  );
 };
 
 export default App;
