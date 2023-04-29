2891e837e8ce8ccf28097cc6ac21d37ae1ec5e2c
Gr4childo <samohkin.2012@gmail.com>
2023-04-29 03:02:14 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TestFromAmoCRM',
+  base: '/Task1-TimerAmo',
   plugins: [react()],
 });
